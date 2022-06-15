45d5f5e45ba832b8c01e499452b724970a2e7a67
abhishek <abhishek2432001@gmail.com>
2022-06-16 02:12:31 +0530

    making first commit of completed major project

---
+++ base/api/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from rest_framework.serializers import ModelSerializer
+from base.models import Room
+
+
+class  RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
